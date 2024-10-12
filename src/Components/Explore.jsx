8677a66896bfa028a4bebb86c535f3d1d571c846
Swapnil Mohite <swapnilmohite9565@gmail.com>
2024-10-12 12:55:11 +0530

    final commit

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -8,30 +8,36 @@ function Explore() {
   const [isClicked,setIsClicked]=useState(false);
   const [inputVal, setinputVal] = useState("");
   const [mealCards, setMealCards] = useState([]);
+ 
 
   const fetchRecipeData = () => {
     axios
       .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputVal}`)
       .then((res) => {
-        console.log(res.data.meals);
-        setMealCards(res.data.meals);
+       
+          console.log(res.data.meals);
+          setMealCards(res.data.meals);
+
+        
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((err) => { 
+     console.error(err);
       });
   };
 
   const handleSearch = () => {
-    setIsClicked(true);
-    if (inputVal === "") {
+    if (inputVal === "" ) {
       alert("Enter dish name first");
-    } else {
+
+    } 
+    else {
       fetchRecipeData();
     }
+    setIsClicked(true);
   };
 
   return (
-    <div className={`w-[641px] ${isClicked && 'h-auto bg-purple-300'}  text-lg sm:w-[768px] md:w-[1024px] lg:w-full`}>
+    <div className={`w-[641px] ${isClicked&&'h-auto bg-purple-300'} ${mealCards===null ?'bg-white':''} text-lg sm:w-[768px] md:w-[1024px] lg:w-full`}>
       <div className='w-full text-center py-5'>
         <input
           className='w-1/2 px-4 py-2 border-2 border-blue-900 rounded-md outline-none sm:w-1/3 md:w-1/4 lg:w-1/5'
@@ -49,7 +55,7 @@ function Explore() {
         </button>
       </div>
       <div className='w-full flex flex-wrap gap-6 justify-center py-10 sm:gap-8 md:gap-10 lg:gap-12'>
-        {mealCards.map((mealCard) => {
+       {mealCards ? (mealCards.map((mealCard) => {
           return (  
               <div key={mealCard.idMeal}
                 className='card text-center w-72 shadow-2xl bg-gray-100 rounded-md p-5 bg-white-300  hover:bg-white sm:w-64 md:w-64 lg:w-60'>
@@ -66,8 +72,8 @@ function Explore() {
                
                 </div>
               </div>  
-          );
-        })}
+          )
+        })):(<h1 className="text-red-700 font-bold text-2xl">Ooops!...Recipe not found</h1>)}
       </div>
     </div>
   );
